cd9e6434b0f55cab542eed6d7e121d79bec8ec6f
Alishah Safdari <alishahsafdari@gmail.com>
2023-06-13 18:24:22 +0430

    Add booksSlice.js file

---
+++ src/redux/books/booksSlice.js
@@ -0,0 +1,20 @@
+import { createSlice } from "@reduxjs/toolkit";
+const initialState = {
+    booksItem:[],
+}
+
+const bookSlice = createSlice({
+    name: 'books',
+    initialState,
+    reducers: {
+        addBook: (state,  action) =>{
+            state.booksItem.push(action.payload);
+        },
+        removeBook: (state, {payload}) =>{
+            state.booksItem = state.booksItem.filter((book) => book.id !== payload.id);
+        },
+    },
+});
+    
+export const { addBook, removeBook} = bookSlice.actions;
+export default bookSlice.reducer;
