6af704f10d85e974e55703a8694a6590e99a9935
Alishah Safdari <alishahsafdari@gmail.com>
2023-06-14 09:59:38 +0430

    Add category input

--- src/components/route/addBook.jsx
+++ src/components/route/addBook.jsx
@@ -1,19 +1,28 @@
 import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import { useSelector, useDispatch } from 'react-redux';
+import { addBook } from '../../redux/books/booksSlice';
 import '../css/bookStyle.css';
 
-const BookForm = ({ addBook }) => {
+const BookForm = () => {
+  const books = useSelector((state) => state.books.booksItem);
+  const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+
   const handleSubmit = (e) => {
     e.preventDefault();
     if (title && author) {
-      addBook({
+      const newBook = {
+        item_id: `item${books.length + 1}`,
         title,
         author,
-      });
+        category,
+      };
+      dispatch(addBook(newBook));
       setTitle('');
       setAuthor('');
+      setCategory('');
     }
   };
   return (
@@ -22,15 +31,17 @@ const BookForm = ({ addBook }) => {
       <form onSubmit={handleSubmit}>
         <input type="text" className="book-title" placeholder="Book title" value={title} onChange={(e) => setTitle(e.target.value)} />
         <input type="text" placeholder="Book author" className="book-author" value={author} onChange={(e) => setAuthor(e.target.value)} />
+        <input
+          type="text"
+          placeholder="Category"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+        />
         <button type="submit">Add Book</button>
       </form>
     </div>
 
   );
-};
-
-BookForm.propTypes = {
-  addBook: PropTypes.func.isRequired,
 };
 
 export default BookForm;
