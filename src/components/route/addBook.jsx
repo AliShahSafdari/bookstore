b38bed642359a66da2ec931ac5b95a92f51ab296
Alishah Safdari <alishahsafdari@gmail.com>
2023-06-15 10:01:57 +0430

    Edit addBook.jsx file

--- src/components/route/addBook.jsx
+++ src/components/route/addBook.jsx
@@ -1,47 +1,37 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { addBook } from '../../redux/books/booksSlice';
-import '../css/bookStyle.css';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import { addBookToApi, addBook } from '../../redux/books/booksSlice';
 
-const BookForm = () => {
-  const books = useSelector((state) => state.books.booksItem);
+const AddBook = () => {
   const dispatch = useDispatch();
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
-  const [category, setCategory] = useState('');
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (title && author) {
-      const newBook = {
-        item_id: `item${books.length + 1}`,
-        title,
-        author,
-        category,
-      };
-      dispatch(addBook(newBook));
-      setTitle('');
-      setAuthor('');
-      setCategory('');
-    }
+  const data = {
+    item_id: uuidv4(),
+    title,
+    author,
+    category: 'Science-fiction',
   };
+  const handleSubmit = () => {
+    dispatch(addBookToApi(data));
+    dispatch(addBook(data));
+    setTitle('');
+    setAuthor('');
+  };
+
   return (
     <div className="book-form">
-      <h2>ADD NEW BOOK</h2>
-      <form onSubmit={handleSubmit}>
-        <input type="text" className="book-title" placeholder="Book title" value={title} onChange={(e) => setTitle(e.target.value)} />
-        <input type="text" placeholder="Book author" className="book-author" value={author} onChange={(e) => setAuthor(e.target.value)} />
-        <input
-          type="text"
-          placeholder="Category"
-          value={category}
-          onChange={(e) => setCategory(e.target.value)}
-        />
-        <button type="submit">Add Book</button>
+      <div>
+        <h3>ADD NEW BOOK</h3>
+      </div>
+      <form>
+        <input value={title} type="text" id="title" placeholder="Book Title Here..." onChange={(e) => setTitle(e.target.value)} />
+        <input value={author} type="text" id="name" placeholder="Author Name Here..." onChange={(e) => setAuthor(e.target.value)} />
+        <button type="button" value="AddNew" onClick={handleSubmit}> AddBook </button>
       </form>
     </div>
-
   );
 };
-
-export default BookForm;
+export default AddBook;
